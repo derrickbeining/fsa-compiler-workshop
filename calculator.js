4b2025c92ce01ea834638a3ced69d2163c9ba238
derrickbeining <derrick.beining@gmail.com>
2017-07-29 15:35:03 -0400

    partner switching

--- calculator.js
+++ calculator.js
@@ -5,7 +5,6 @@ function Calculator(input) {
 function TreeNode(name, ...children) {
   this.name = name;
   this.children = children;
-
 }
 
 Calculator.prototype.lexer = function(str) {
@@ -31,13 +30,11 @@ Calculator.prototype.lexer = function(str) {
       throw new Error('Found uparseable token: ' + str[i])
     }
   }
-  console.log(stream);
-
   return stream
 }
 
-Calculator.prototype.peek = function(currentIndex) {
-  return this.tokenStream[currentIndex + 1] || null;
+Calculator.prototype.peek = function() {
+  return this.tokenStream[0] || null;
 }
 
 Calculator.prototype.get = function() {
@@ -45,14 +42,72 @@ Calculator.prototype.get = function() {
 }
 
 Calculator.prototype.parseExpression = function() {
-  var a = parseA();
-  var t = parseTerm();
+  var t = this.parseTerm();
+  var a = this.parseA();
   return new TreeNode('Expression', t, a);
 }
 
+Calculator.prototype.parseTerm = function() {
+  const f = this.parseF()
+  const b = this.parseB()
+  return new TreeNode('Term', f, b)
+}
 
-const calc = new Calculator('1+(2*3)+4')
-var num = calc.get();
-var num2 = calc.get();
-console.log(num2);
+Calculator.prototype.parseA = function() {
+  const nextToken = this.peek();
+  if (nextToken && nextToken.name === 'ADD') {
+    this.get();
+    return  new TreeNode('A', '+', this.parseTerm(), this.parseA())
+  } else if (nextToken && nextToken.name === 'SUB') {
+    this.get();
+    return new TreeNode('A', '-', this.parseTerm(), this.parseA())
+  } else {
+    return new TreeNode('A')
+  }
+}
+
+Calculator.prototype.parseB = function() {
+  const nextToken = this.peek();
+  if (nextToken && nextToken.name === 'MUL') {
+    this.get();
+    return  new TreeNode('B', '*', this.parseF(), this.parseB())
+  } else if (nextToken && nextToken.name === 'DIV') {
+    this.get();
+    return new TreeNode('B', '/', this.parseF(), this.parseB())
+  } else {
+    return new TreeNode('B')
+  }
+}
+
+Calculator.prototype.parseF = function() {
+  const nextToken = this.peek();
+  if (nextToken && nextToken.name === 'LPAREN') {
+    this.get();
+    const expr = this.parseExpression();
+    this.get();
+    return  new TreeNode('F', '(', expr, ')')
+  } else if (nextToken && nextToken.name === 'SUB') {
+    return new TreeNode('F', '-', this.parseF())
+  } else {
+    return new TreeNode('F', nextToken.value)
+  }
+}
+
+// E = Expression
+// T = Term
+// F = Factor
+// A = ExpressionRemainder // a placeholder created to remove the left-recursion
+// B = TermRemainder // same as above
+
+// E => T A
+// A => + T A
+//      - T A
+//      epsilon
+// T => F B
+// B => * F B
+//      / F B
+//      epsilon
+// F => ( E )
+//      - F
+//      NUMBER
 
